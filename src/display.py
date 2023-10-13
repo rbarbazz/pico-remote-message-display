2e80378efbb522b2c2ca0a741967cb51b8e5a2be
rbarbazz <42906704+rbarbazz@users.noreply.github.com>
2023-10-12 23:53:25 -0700

    feat: proper writing, multi line and multi screen support

--- src/display.py
+++ src/display.py
@@ -1,17 +1,19 @@
+import math
+
 from picographics import PicoGraphics, DISPLAY_PICO_DISPLAY_2, PEN_P4
 from pimoroni import RGBLED
-
 
 # Constants
 RGB_GREEN = (0, 255, 0)
 RGB_BLACK = (0, 0, 0)
+FONT_HEIGHT = 8
+FONT_SCALE = 4
+SCALED_FONT_HEIGHT = FONT_HEIGHT * FONT_SCALE
+PADDING = 10
+LINE_HEIGHT = SCALED_FONT_HEIGHT + PADDING
 
 
 class Display:
-    FONT_HEIGHT = 8
-    FONT_SCALE = 6
-    SCALED_FONT_HEIGHT = FONT_HEIGHT * FONT_SCALE
-
     def __init__(self):
         self.display = PicoGraphics(
             display=DISPLAY_PICO_DISPLAY_2, pen_type=PEN_P4, rotate=0
@@ -33,20 +35,27 @@ class Display:
     def turn_led_on(self):
         self.led.set_rgb(*RGB_GREEN)
 
-    def write_text(self, text: str):
-        text_width = self.display.measure_text(text, scale=Display.FONT_SCALE)
-        DISPLAY_WIDTH, DISPLAY_HEIGHT = self.bounds
+    @property
+    def max_lines(self):
+        _, display_height = self.bounds
 
+        return math.floor(display_height / LINE_HEIGHT)
+
+    def get_text_width(self, text: str):
+        return self.display.measure_text(text, scale=FONT_SCALE)
+
+    def fits_on_display(self, text: str):
+        display_width, _ = self.bounds
+        text_width = self.get_text_width(text=text)
+
+        return display_width - PADDING * 2 - text_width >= 0
+
+    def write_text(self, text: str, line_index: int):
         self.display.set_pen(self.green_pen)
         self.display.text(
             text,
-            # Center the text horizontally
-            int((DISPLAY_WIDTH - text_width) / 2),
-            int(
-                # Center the text vertically
-                (DISPLAY_HEIGHT - Display.SCALED_FONT_HEIGHT)
-                / 2
-            ),
-            scale=Display.FONT_SCALE,
+            PADDING,
+            (LINE_HEIGHT * line_index),
+            scale=FONT_SCALE,
         )
         self.display.update()
--- src/tasks.py
+++ src/tasks.py
@@ -1,8 +1,32 @@
-from display import Display
 import asyncio
+import random
+
+from display import Display
+
+
+SPEED = 60
 
 display = Display()
 lock = asyncio.Lock()
+
+
+def split_text(text: str):
+    lines = []
+    current_line = ""
+
+    for char in text:
+        if not current_line:
+            current_line = char
+        elif display.fits_on_display(current_line + char):
+            current_line += char
+        else:
+            lines.append(current_line.strip())
+            current_line = char
+
+    if current_line:
+        lines.append(current_line)
+
+    return lines
 
 
 async def process_message(message: str):
@@ -10,9 +34,28 @@ async def process_message(message: str):
         # Wait one second to avoid blinking effect between messages
         await asyncio.sleep(1)
 
-        display.turn_led_on()
-        display.write_text(message)
-        await asyncio.sleep(5)
+        # Split message into lines
+        message_lines = split_text(message)
+        max_lines = display.max_lines
+        # Split the lines into screens based on max lines per screen value
+        screens = [
+            message_lines[i : i + max_lines]
+            for i in range(0, len(message_lines), max_lines)
+        ]
 
+        display.turn_led_on()
+
+        for screen in screens:
+            for line_index, line in enumerate(screen):
+                for i in range(len(line)):
+                    display.write_text(line[: i + 1], line_index=line_index)
+                    # Simulate human typing
+                    await asyncio.sleep_ms(int(SPEED + SPEED * (random.random() - 0.5)))
+
+            # Wait and move onto the next screen
+            await asyncio.sleep(1)
+            display.clear_text()
+
+        await asyncio.sleep(2)
         display.clear_text()
         display.clear_led()
